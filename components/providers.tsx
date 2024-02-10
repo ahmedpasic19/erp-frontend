f9cdeb9e53b82e903f857a178068ebc8aacd29a9
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-02-10 15:02:46 +0100

    Setup RTK Query & Redux
    Changes:
    - Installed redux-toolkit
    - Installed react-redux
    - Setup store & RTK query

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,13 +3,20 @@
 import { ReactNode } from 'react'
 
 import { SessionProvider } from 'next-auth/react'
+import { Provider } from 'react-redux'
+
+import { store } from '@/app/_features/store'
 
 type TProps = {
    children: ReactNode
 }
 
 const Providers = ({ children }: TProps) => {
-   return <SessionProvider>{children}</SessionProvider>
+   return (
+      <SessionProvider>
+         <Provider store={store}>{children}</Provider>
+      </SessionProvider>
+   )
 }
 
 export default Providers
